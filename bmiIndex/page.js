fb15d5c09e764fcbef7297571bba2d949b399eea
leedongdong2 <77323959+leedongdong2@users.noreply.github.com>
2021-05-06 16:57:30 +0900

    Add files via upload

---
+++ bmiIndex/page.js
@@ -0,0 +1,9 @@
+//id
+const login = document.getElementById("login");
+const firstbox = document.getElementById("firstbox");
+const secondbox = document.getElementById("secondbox")
+
+//style
+const sizeout = "sizeout";
+const disnone = "disnone";
+
